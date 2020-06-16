12d4bc189b674818c225ffa4735ad7d27a9031b9
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-06-16 21:27:26 +0300

    hm

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,53 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import "./App.css";
 
 // components
 import InputTodo from "./components/InputTodo";
 import ListTodos from "./components/ListTodos";
 
-function App() {
-  return (
-    <Fragment>
-      <div className="container">
-        <InputTodo />
-        <ListTodos />
-      </div>
-    </Fragment>
-  );
-}
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: 'McDev'
+    }
+  }
+
+  getData() {
+    setTimeout(() => {
+      console.log('Our data is fetched');
+      this.setState({
+        data: 'Hello McDev'
+      })
+    }, 1000)
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="container">
+          <InputTodo />
+          <ListTodos />
+        </div>
+      </Fragment>
+    );
+  }
+
+
+// function App() {
+//   return (
+//     <Fragment>
+//       <div className="container">
+//         <InputTodo />
+//         <ListTodos />
+//       </div>
+//     </Fragment>
+//   );
+// }
 
 export default App;
