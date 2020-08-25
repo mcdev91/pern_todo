6dc5ba3986cf617d13314ae1a0ab03db20ab23a9
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-08-25 20:41:55 +0300

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,9 @@
 import React, { Fragment, Component } from "react";
 import "./App.css";
-
-// components
 import InputTodo from "./components/InputTodo";
 import ListTodos from "./components/ListTodos";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: 'McDev'
-    }
-  }
-
-  // getData() {
-  //   setTimeout(() => {
-  //     const password = prompt('prisijunkite');
-  //     if (password === 'family') {
-  //       alert('start your shop list');
-  //     } else { this.getData() }
-  //     this.setState({
-  //       data: 'Hello McDev'
-  //     })
-  //   }, 1000)
-  // }
-
-  // componentDidMount() {
-  //   this.getData();
-  // }
 
   render() {
     return (
@@ -41,16 +17,5 @@ class App extends Component {
   }
 
 }
-
-// function App() {
-//   return (
-//     <Fragment>
-//       <div className="container">
-//         <InputTodo />
-//         <ListTodos />
-//       </div>
-//     </Fragment>
-//   );
-// }
 
 export default App;
